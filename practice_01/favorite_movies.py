21bb6bbb7e085fa5f04b762e2b09dd6e7c342a7f
Mitrich-23 <ivsura_navi_2307_7777@mail.ru>
2023-01-20 18:40:48 +0700

    дз

--- practice_01/favorite_movies.py
+++ practice_01/favorite_movies.py
@@ -1,6 +1,10 @@
 # Есть строка с перечислением фильмов
 
 my_favorite_movies = 'Терминатор, Пятый элемент, Аватар, Чужие, Назад в будущее'
+print(my_favorite_movies[0:10])
+print(my_favorite_movies[42:57])
+print(my_favorite_movies[12:25])
+print(my_favorite_movies[-22:-17])
 
 # Выведите на консоль с помощью индексации строки, последовательно:
 #   первый фильм
