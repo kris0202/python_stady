2bea1b9d8d7d14fcfbe5ae9e3d0dac51483021a2
Mitrich-23 <ivsura_navi_2307_7777@mail.ru>
2023-02-17 19:15:21 +0700

    дз

--- practice_01/songs_list.py
+++ practice_01/songs_list.py
@@ -15,13 +15,18 @@ violator_songs_list = [
     ['Clean', 5.83],
 ]
 
+count = violator_songs_list[3][1] + violator_songs_list[5][1] + violator_songs_list[8][1]
+count = round(count, 2)
+
+print(f'Три песни звучат {count} минут')
+
 # распечатайте общее время звучания трех песен: 'Halo', 'Enjoy the Silence' и 'Clean' в формате
 #   Три песни звучат ХХХ.XX минут
 # Обратите внимание, что делать много вычислений внутри print() - плохой стиль.
 # Лучше заранее вычислить необходимое, а затем в print(xxx, yyy, zzz)
 
 
-print(f'Три песни звучат {} минут')
+#print(f'Три песни звучат {} минут')
 
 # Есть словарь песен группы Depeche Mode
 violator_songs_dict = {
@@ -39,5 +44,6 @@ violator_songs_dict = {
 # распечатайте общее время звучания трех песен: 'Sweetest Perfection', 'Policy of Truth' и 'Blue Dress'
 #   А другие три песни звучат ХХХ минут
 
+music = violator_songs_dict['Sweetest Perfection'] + violator_songs_dict['Policy of Truth'] + violator_songs_dict['Blue Dress']
 
-print(f'А другие три песни звучат {round(length_1)} минут')
+print(f'А другие три песни звучат {round(music, 2)} минут')
