2a2354bc88b91d8250d53950d280cf04bbca545d
Mitrich-23 <ivsura_navi_2307_7777@mail.ru>
2023-02-17 19:16:06 +0700

    дз

--- practice_01/store.py
+++ practice_01/store.py
@@ -38,7 +38,24 @@ lamps_item = store[lamp_code][0]
 lamps_quantity = lamps_item['quantity']
 lamps_price = lamps_item['price']
 lamps_cost = lamps_quantity * lamps_price
-print('Лампа -', lamps_quantity, 'шт, стоимость', lamps_cost, 'руб')
+#print('Лампа -', lamps_quantity, 'шт, стоимость', lamps_cost, 'руб')
+
+table_item_1 = store[goods['Стол']][0]
+table_item_2 = store[goods['Стол']][1]
+price_tables = table_item_1['quantity'] * table_item_1['price'] + table_item_2['quantity'] * table_item_2['price']
+
+print(f'Стол - {table_item_1["quantity"] + table_item_2["quantity"]} шт, стоимость {table_item_1["price"] + table_item_2["price"]} руб')
+
+sofa_item_1 = store[goods['Диван']][0]
+sofa_item_2 = store[goods['Диван']][1]
+
+print(f'Диван - {sofa_item_1["quantity"] + sofa_item_2["quantity"]} шт, стоимость {sofa_item_1["price"] + sofa_item_2["price"]} руб')
+
+chair_item_1 = store[goods['Стул']][0]
+chair_item_2 = store[goods['Стул']][1]
+chair_item_3 = store[goods['Стул']][2]
+
+print(f'Стул - {chair_item_1["quantity"] + chair_item_2["quantity"] + chair_item_3["quantity"]} шт, стоимость {chair_item_1["price"] + chair_item_2["price"] + chair_item_3["price"]} руб')
 
 # Вывести стоимость каждого вида товара на складе:
 # один раз распечать сколько всего столов и их общая стоимость,
